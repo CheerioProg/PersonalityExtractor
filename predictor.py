8e70bee1e5bda4cc8ca8e7e9d70acd8e9834c376
CheerioProg <93529431+CheerioProg@users.noreply.github.com>
2022-09-25 11:32:34 +0800

    Add files via upload

---
+++ predictor.py
@@ -0,0 +1,180 @@
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=DeprecationWarning)
+    import re
+    import time
+    import svm
+    import pandas as pd
+
+    import numpy as np
+    import joblib
+    from bert_serving.client import BertClient
+    import os.path
+    from joblib import dump, load
+
+def shrink_text(text, clean_string=True):
+    status = []
+    sentences = re.split(r'[.]', text.strip())
+    # print(sentences)
+    try:
+        sentences.remove('')
+    except ValueError:
+        pass
+    sentences = [sent + "." for sent in sentences]
+    last_sentences = []
+    for i in range(len(sentences)):
+        sents = re.split(r'[?]', sentences[i].strip())
+        for s in sents:
+            try:
+                if len(s) == 0:
+                    pass
+                elif s[-1] == ".":
+                    last_sentences.append(s)
+                else:
+                    last_sentences.append(s + "?")
+            except Exception as e:
+                print(s)
+    sentences = last_sentences
+    x = 0
+    for sent in sentences:
+        if clean_string:
+            orig_rev = sent.strip()
+            if orig_rev == '':
+                continue
+            splitted = orig_rev.split()
+            x += len(splitted)
+            if len(splitted) > 200:
+                orig_rev = []
+                splits = int(np.floor(len(splitted) / 200))
+                for index in range(splits):
+                    orig_rev.append(' '.join(splitted[index * 200:(index + 1) * 200]))
+                if len(splitted) > splits * 200:
+                    orig_rev.append(' '.join(splitted[splits * 200:]))
+                status.extend(orig_rev)
+            else:
+                status.append(orig_rev)
+        else:
+            orig_rev = sent.strip().lower()
+            status.append(orig_rev)
+    return status
+
+
+def build_new_chunks(sentences):
+    now_len = 0
+    text = []
+    text_chunks = []
+    for idx, sent in enumerate(sentences):
+        now_len += len(sent.split())
+        if now_len <= 200:
+            text.append(sent)
+        if now_len > 200 or idx == len(sentences) - 1:
+            x = True
+            txt = " ".join(text)
+            text_chunks.append(txt.replace("\"", "\"\""))
+            text = [sent]
+            now_len = len(sent.split())
+    return text_chunks
+
+
+def preocess_chunks(text_chunks, clean_string=True):
+    revs = []
+    for chunk in text_chunks:
+        status = []
+        sentences = chunk.strip()
+        if clean_string:
+            orig_rev = clean_str(sentences.strip())
+            if orig_rev == '':
+                continue
+            splitted = orig_rev.split()
+            if len(splitted) > 250:
+                orig_rev = []
+                splits = int(np.floor(len(splitted) / 250))
+                for index in range(splits):
+                    orig_rev.append(' '.join(splitted[index * 250:(index + 1) * 250]))
+                if len(splitted) > splits * 250:
+                    orig_rev.append(' '.join(splitted[splits * 250:]))
+                status.extend(orig_rev)
+            else:
+                status.append(orig_rev)
+        else:
+            orig_rev = sentences.strip().lower()
+            status.append(orig_rev)
+
+        revs.append(status)
+    return revs
+
+
+def clean_str(string, TREC=False):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Every dataset is lower cased except for TREC
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!.?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s ", string)
+    string = re.sub(r"\'ve", " have ", string)
+    string = re.sub(r"n\'t", " not ", string)
+    string = re.sub(r"\'re", " are ", string)
+    string = re.sub(r"\'d", " would ", string)
+    string = re.sub(r"\'ll", " will ", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\.", " . ", string)
+    string = re.sub(r"\(", " ( ", string)
+    string = re.sub(r"\)", " ) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    #    string = re.sub(r"[a-zA-Z]{4,}", "", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip() if TREC else string.strip().lower()
+
+
+def load_bert_vec(processed_chunks):
+    bc = BertClient()
+    embeddings = []
+
+    for rev_idx, rev in enumerate(processed_chunks):
+        rev_splitted = [orig_rev.split() for orig_rev in rev]
+        result = bc.encode(rev_splitted, is_tokenized=True)
+        embeddings.append(result)
+    return embeddings
+
+
+def predict_with_model(embeddings, l=0):
+    embeddings4layers = []
+    for embedding in embeddings:
+        embeddings4layers.append(np.mean(embedding[:, -768 * 4:], axis=0))
+    results = []
+    for y in range(0, 5):
+        model_file_name = 'svm_model_y'+str(y)+'.joblib'
+        if os.path.isfile('/content/drive/MyDrive/PersonalityExtractor/'+ model_file_name):
+            classifier = load('/content/drive/MyDrive/PersonalityExtractor/'+ model_file_name)
+        else:
+             classifier, _, _, _ = svm.classify("/content/drive/MyDrive/PersonalityExtractor/essays_mairesse_sb_tokenized_200_max_rev_vector.p", y, -1, 0, add_mairesse=False)
+             dump(classifier, model_file_name)
+        predicts = classifier.predict_proba(embeddings4layers)[:,1]
+        mean = np.max(predicts)
+        results.append(mean)
+    return results
+
+
+def predict(text):
+    sentences = shrink_text(text)
+    chunks = build_new_chunks(sentences)
+    processed_chunks = preocess_chunks(chunks)
+    embeddings = load_bert_vec(processed_chunks)
+    results = predict_with_model(embeddings)
+    return results
+
+
+if __name__ == "__main__":
+    text_input = pd.read_csv('/content/PersonalityExtractor/DataSetCOMPLETE.csv')
+    txt_input = text_input['text'].iloc[10001:15000]
+    li = []
+    for i in txt_input:
+      x = i
+      predictions = predict(x)
+      print(predictions)
+      li.append(predictions) 
+      output = pd.DataFrame()
+      output = pd.DataFrame(li)
+    outputCSV = output.to_csv('/content/PersonalityExtractor/DataSetBIG5(1).csv', encoding='utf-8', index=False)
